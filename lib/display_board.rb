89a17fc49e79eb500e460ffcb4499951c33fd310
Callum Walsh <makomirocket@gmail.com>
2019-10-06 10:51:23 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,9 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 
-def display_board (b)
-  puts " #{b[0]}  | #{b[1]}  | #{b[2]}  "
+def display_board
+  puts "   |   |   "
   puts "-----------"
-  puts " #{b[3]}  | #{b[4]}  | #{b[5]}  "
+  puts "   |   |   "
   puts "-----------"
-  puts " #{b[6]}  | #{b[7]}  | #{b[8]}  "
+  puts "   |   |   "
 end
-
-display_board = " ", " ", " ", " ", " ", " ", " ", " ", " "
